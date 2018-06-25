35a60772de57ea80d9d8a867d4dc672a5d0fdd3b
sanghyuhoh <jpc8800@pusan.ac.kr>
2018-06-25 12:09:46 +0000

    third commit

--- postnget.1.js
+++ postnget.1.js
@@ -20,6 +20,7 @@ module.exports = function(app, pool) {
         var option2price = array["option2price"];
         var option3 = array["option3"];
         var option3price = array["option3price"];
+        var totalorder = "0";
         // 상기동일
         var attr = req.body.attr;
         
@@ -30,16 +31,17 @@ module.exports = function(app, pool) {
             var productid = "";
             pool.getConnection(function(err, conn) {
                 // title 정보를 DB에 넣기 위한 SQL문 준비
-                var sql1 = "SELECT COUNT(*) AS count from productinfo;";
-                 
+                var sql1 = "select productid from productinfo order by productid desc limit 1;";
+                
                 var query = conn.query(sql1, function(err, result) {
-                    productid = result[0].count;
+                    
+                    productid = parseInt(result[0].productid) + parseInt("1");
                     mes = productid;
                     console.log(mes);
                     
                 var sql = "INSERT INTO productinfo (productid, productname, price, count, option1, option1price, option2, option2price, option3," +
-                "option3price) VALUES ('" + productid + "', '" + productname + "', '" + price + "', '" + count + "', '" + option1 + "'," +
-                "'" + option1price + "', '" + option2 + "', '" + option2price + "', '" + option3 + "', '" + option3price + "');";
+                "option3price, totalorder) VALUES ('" + productid + "', '" + productname + "', '" + price + "', '" + count + "', '" + option1 + "'," +
+                "'" + option1price + "', '" + option2 + "', '" + option2price + "', '" + option3 + "', '" + option3price + "', '" + totalorder +"');";
                 conn.query(sql, function(err) {
                     // err가 떠도 conn은 release() 꼭 해주어야한다
                     
@@ -178,9 +180,14 @@ module.exports = function(app, pool) {
         var option2 = array["option2"];
         var option3 = array["option3"];
         var totalPrice;
+        //주문시간 추가
+
+        //var newDate = new Date();
+        var dateFormat = require('dateformat');
+        var orderdate = dateFormat(new Date(), "yyyy-mm-dd");
         if (productid == undefined) {
             result = returnResult(new Error("2"+result), res);
-            result.isSuccess = '0';
+            result.isSuccess = '2';
         }
         else {
             pool.getConnection(function(err, conn) {
@@ -188,15 +195,19 @@ module.exports = function(app, pool) {
                 conn.query(sql, function(err, rows) {
                     if (rows) {
                         if(rows[0] == null){
-                            result.isSuccess = '0';
+                            result = returnResult(err, res);
+                            result.isSuccess = "2";
                             console.log("쿼리실패");
                             res.send(result);
+                            conn.release();
                         }
                         else{
                             if(rows[0].count < count){
-                                result.isSuccess = '0';
+                                result = returnResult(err, res);
+                                result.isSuccess = "0";
                                 console.log("재고부족");
                                 res.send(result);
+                                conn.release();
                             }
                             else{
                                 var result = {};
@@ -217,25 +228,42 @@ module.exports = function(app, pool) {
                                     totalPrice += parseInt(rows[0].option3price);
                                 }
                                 totalPrice *= parseInt(count);
-                                var sql = "INSERT INTO salesinfo (productID, productname, count, option1, option2, option3, price) VALUES ('" +
-                                productid + "', '" + productname + "', '" + count +"', '" + option1 +"', '" + option2 + "', '" + option3 + "', '"  + totalPrice + "');";
-                                conn.query(sql, function(err) {
+                                
+                                var sql = "INSERT INTO salesinfo (productID, productname, count, option1, option2, option3, price, orderdate) VALUES ('" +
+                                productid + "', '" + productname + "', '" + count +"', '" + option1 +"', '" + option2 + "', '" + option3 + "', '"  + totalPrice + "', '" + orderdate + "');"
+                                "select totalorder from productinfo where productid= \"" + productid + "\";";
+                                conn.query(sql, function(err, rows) {
+                                });
+                                    var array = rows;
+                                    var totalorder = rows[0]["totalorder"];
+                                    console.log(totalorder);
+                                    var totalordervar = parseInt(totalorder) + parseInt(count);
+                                    var sql2 = "UPDATE productinfo SET totalorder = '"+ totalordervar +"' where productid= \"" + productid +"\";";
+                                    console.log(totalordervar);
+                                    conn.query(sql2, function(err) {
+                                    });
+                                    
                                     if(err){
                                         console.log("에러");
                                     }
-                                });
-                                result.isSuccess = '1';
+
+                                
+                                result = returnResult(err, res);
+                                result.isSuccess = "1";
                                 console.log("성공");
                                 res.send(result);
+                                conn.release();
                             }
-                        }
+                        }//res.send(result);
                     }
                     else{
-                        result.isSuccess = '0';
+                        result = returnResult(err, res);
+                        result.isSuccess = "2";
                         console.log("실패");
                         res.send(result);
+                        conn.release();
                     }
-                    conn.release();
+                    //conn.release();
                 });
             });
         }
@@ -246,6 +274,26 @@ module.exports = function(app, pool) {
          var result = {};
          pool.getConnection(function(err, conn) {
              var sql = "SELECT * from salesinfo;";
+        	 conn.query(sql, function(err, rows) {
+                var result = returnResult(err, res);
+                if (rows) {
+                    result.message = rows;
+                }
+                conn.release();
+                result.status = res.statusCode;
+                res.send(result);
+            });
+        });
+     });
+     
+     //OrderedProductListManager By Date
+     app.post("/orderinfobydate",function(req,res){
+         var result = {};
+         var array = req.body;
+         var startdate = array["startdate"];
+         var enddate = array["enddate"];
+         pool.getConnection(function(err, conn) {
+             var sql = "SELECT * from salesinfo where orderdate between '" + startdate + "' AND '" + enddate +"';";
         	 conn.query(sql, function(err, rows) {
                 var result = returnResult(err, res);
                 if (rows) {
@@ -294,6 +342,59 @@ module.exports = function(app, pool) {
         });
     });
     
+    //제품 전체 리스트 재고량순 주기
+    // >> GET
+    app.get("/searchproductallbycount", function(req, res) {
+	var result = {};
+        // db에 연결하여 sql 수행
+        pool.getConnection(function(err, conn) {
+        	var sql = "SELECT * from productinfo order by totalorder desc;";
+        	//select productid from productinfo order by productid desc limit 1;
+            conn.query(sql, function(err, rows) {
+                var result = returnResult(err, res);
+                if (rows) {
+                    var resultArray = new Array;
+                    for(var x = 0; x<rows.length; x++){
+                    var tempresultArray = new Array;
+                    tempresultArray = ["productid", rows[x]["productid"], "productname",  rows[x]["productname"]
+                    , "price",  rows[x]["price"], "count",  rows[x]["count"]];
+                    resultArray.push(tempresultArray);
+                    }
+                    result.message = resultArray;
+                }
+                conn.release();
+                result.status = res.statusCode;
+                res.send(result);
+            });
+        });
+    });   
+    
+     //제품 전체 리스트 최신순 주기
+    // >> GET
+    app.get("/searchproductallbyrecent", function(req, res) {
+	var result = {};
+        // db에 연결하여 sql 수행
+        pool.getConnection(function(err, conn) {
+        	var sql = "SELECT * from productinfo order by productid desc;";
+        	//select productid from productinfo order by productid desc limit 1;
+            conn.query(sql, function(err, rows) {
+                var result = returnResult(err, res);
+                if (rows) {
+                    var resultArray = new Array;
+                    for(var x = 0; x<rows.length; x++){
+                    var tempresultArray = new Array;
+                    tempresultArray = ["productid", rows[x]["productid"], "productname",  rows[x]["productname"]
+                    , "price",  rows[x]["price"], "count",  rows[x]["count"]];
+                    resultArray.push(tempresultArray);
+                    }
+                    result.message = resultArray;
+                }
+                conn.release();
+                result.status = res.statusCode;
+                res.send(result);
+            });
+        });
+    });   
     
     //제품 전체 리스트
     // >> GET
@@ -330,6 +431,66 @@ module.exports = function(app, pool) {
         // db에 연결하여 sql 수행
         pool.getConnection(function(err, conn) {
         	var sql = "SELECT * from productinfo WHERE productname like '%" + name + "%';";
+        	console.log(name);
+            conn.query(sql, function(err, rows) {
+                var result = returnResult(err, res);
+                if (rows) {
+                    var resultArray = new Array;
+                    for(var x = 0; x<rows.length; x++){
+                    var tempresultArray = new Array;
+                    tempresultArray = ["productid", rows[x]["productid"], "productname",  rows[x]["productname"]
+                    , "price",  rows[x]["price"], "count",  rows[x]["count"]];
+                    resultArray.push(tempresultArray);
+                    }
+                    result.message = resultArray;
+                }
+                conn.release();
+                result.status = res.statusCode;
+                res.send(result);
+            });
+        });
+    });
+    
+    //productSearchManager 구현
+     //제품 이름으로 검색하기 주문많은순
+    app.post("/searchbytotalorder", function(req, res) {
+    var array = req.body;
+    var name = array["name"];
+	var result = {};
+        // db에 연결하여 sql 수행
+        pool.getConnection(function(err, conn) {
+        	var sql = "SELECT * from productinfo WHERE productname like '%" + name + "%' order by totalorder desc;";
+        	console.log(name);
+            conn.query(sql, function(err, rows) {
+                var result = returnResult(err, res);
+                if (rows) {
+                    var resultArray = new Array;
+                    for(var x = 0; x<rows.length; x++){
+                    var tempresultArray = new Array;
+                    tempresultArray = ["productid", rows[x]["productid"], "productname",  rows[x]["productname"]
+                    , "price",  rows[x]["price"], "count",  rows[x]["count"]];
+                    resultArray.push(tempresultArray);
+                    }
+                    result.message = resultArray;
+                }
+                conn.release();
+                result.status = res.statusCode;
+                res.send(result);
+            });
+        });
+    });
+	
+	//productSearchManager 구현
+     //제품 이름으로 검색하기 최신순
+    app.post("/searchbyrecent", function(req, res) {
+    var array = req.body;
+    var name = array["name"];
+	var result = {};
+        // db에 연결하여 sql 수행
+        pool.getConnection(function(err, conn) {
+        	var sql = "SELECT * from productinfo WHERE productname like '%" + name + "%' order by productid desc;";
+        	//"select productid from productinfo order by productid desc limit 1
+        	console.log(name);
             conn.query(sql, function(err, rows) {
                 var result = returnResult(err, res);
                 if (rows) {
